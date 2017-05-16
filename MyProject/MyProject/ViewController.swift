2742e4e70db631208d60f305b6d3dba6defc0e38
Fuji <fuji@Fujis-iMac.local>
2017-05-16 11:28:59 +0700

    oki1

--- MyProject/MyProject/ViewController.swift
+++ MyProject/MyProject/ViewController.swift
@@ -13,23 +13,17 @@ class ViewController: UIViewController {
     
     var tuan1: Tuan1? = nil
     
+    @IBAction func logginGoogle(_ sender: Any) {
+        tuan1?.login()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
        Tuan().hello(vc: self)
-        
         tuan1 = Tuan1.init(vc: self)
        
-    }
-
-
-    override func viewDidAppear(_ animated: Bool) {
-        
-        print("xxxxx")
-
-        tuan1?.login()
     }
 
 }
